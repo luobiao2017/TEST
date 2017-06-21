a599ce8817c70dc38ebdcbe6e620ead300514010
luobiao2017 <693016926@qq.com>
2017-06-21 19:30:30 +0800

    add

---
+++ js/home.js
@@ -0,0 +1,22 @@
+$(function(){
+	
+	//第一屏
+	var n=0;
+	$("#section_menu")[0].addEventListener("touchstart",function(ev){//滑动菜单
+		ev.preventDefault();
+	})
+	var w=$(window).width();
+	$("#section_menu").on("swipeLeft",function(){
+		n++;if(n>=1){n=1;}
+	    $("#mini_link").css("-webkit-transform","translate3d("+(-n*50)+"%,0,0)");
+	}).on("swipeRight",function(){
+		n--;if(n<=0){n=0;}
+	    $("#mini_link").css("-webkit-transform","translate3d("+(-n*50)+"%,0,0)");
+	})
+	
+	
+	
+	
+	
+	
+})
